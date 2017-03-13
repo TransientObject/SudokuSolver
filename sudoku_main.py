8b08c56e0b92ccfd6732e75c626b75c7083ffcf8
Priya PNS <mail2priyanarayanan@gmail.com>
2017-03-12 18:26:30 -0700

    adding backtracking algo

--- sudoku_main.py
+++ sudoku_main.py
@@ -1,5 +1,38 @@
 from sudoku_csp import *
 from sudoku_csp_optimized import *
+from sudoku_backtracking import *
+
+
+def solve(method, start_state):
+    if (method == 'a'):
+        sudoku = SudokuCSPOptimized(int(math.sqrt(len(start_state))))
+        goal = sudoku.solve(sys.argv[2])
+        sudoku.display(goal)
+    elif(method == 'b'):
+        sudoku = SudokuCSP(int(math.sqrt(len(start_state))))
+        result = sudoku.csp_solve(sys.argv[2])
+        if result:
+            goal = (collections.OrderedDict(sorted(result.items()))).values()
+            print(goal)
+            sudoku.display(goal)
+        else:
+            print("The puzzle can't be solved using only current CSP strategies.")
+    elif(method == 'c'):
+        sudoku = SudokuCSP(int(math.sqrt(len(start_state))))
+        result = sudoku.csp_dfs_solve(sys.argv[2])
+        goal = (collections.OrderedDict(sorted(result.items()))).values()
+        print(goal)
+        sudoku.display(goal)
+    elif(method == 'd'):
+        sudoku = SudokuBackTracking(int(math.sqrt(len(start_state))), start_state)
+        if (sudoku.solve_sudoku()):
+            sudoku.display(''.join(sudoku.possible_values.values()))
+        else:
+            print("No solution exists")
+    else:
+        print("invalid algorithm choice. Enter a/b/c")
+        exit(0)
+
 
 if __name__=="__main__":
     if(len(sys.argv) < 3):
@@ -12,25 +45,4 @@ if __name__=="__main__":
         print("sudoku input is incorrect. should be a perfect n ^ 4")
         exit(0)
 
-    if (sys.argv[1] == 'a'):
-        sudoku = SudokuCSPOptimized(int(math.sqrt(len(start_state))))
-        goal = sudoku.solve(sys.argv[2])
-        sudoku.display(goal)
-    elif(sys.argv[1] == 'b'):
-        sudoku = SudokuCSP(int(math.sqrt(len(start_state))))
-        result = sudoku.csp_solve(sys.argv[2])
-        if result:
-            goal = (collections.OrderedDict(sorted(result.items()))).values()
-            print(goal)
-            sudoku.display(goal)
-        else:
-            print("The puzzle can't be solved using only current CSP strategies.")
-    elif(sys.argv[1] == 'c'):
-        sudoku = SudokuCSP(int(math.sqrt(len(start_state))))
-        result = sudoku.csp_dfs_solve(sys.argv[2])
-        goal = (collections.OrderedDict(sorted(result.items()))).values()
-        print(goal)
-        sudoku.display(goal)
-    else:
-        print("invalid algorithm choice. Enter a/b/c")
-        exit(0)
+    solve(sys.argv[1], start_state)
